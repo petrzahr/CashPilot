8c46c6a13fac0793c2d34bddc8a77941c741fe53
petrzahr <pzahradn@gmail.com>
2026-09-12 20:36:32 +0200

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-﻿import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/CashPilot/',
   plugins: [react()],
   server: {
     port: 3000,
